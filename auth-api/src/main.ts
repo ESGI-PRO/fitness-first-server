bef6b035145f7631084ca71a328d1548bace2b34
Adam SELLIN <adamsellin1914@gmail.com>
2023-04-12 12:47:55 +0200

    add package microservice for auth

--- auth-api/src/main.ts
+++ auth-api/src/main.ts
@@ -1,21 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 import helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: true,
-    logger: ['error', 'warn'],
-  });
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      cors: true,
+      logger: ['error', 'warn'],
+      transport: Transport.TCP,
+    },
+  );
 
-const config = new DocumentBuilder()
-  .setTitle('Auth API')
-  .setDescription('Auth API description')
-  .setVersion('1.0')
-  .build();
-const document = SwaggerModule.createDocument(app, config);
-SwaggerModule.setup('api-auth', app, document);
+  const config = new DocumentBuilder()
+    .setTitle('Auth API')
+    .setDescription('Auth API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   app.use(helmet());
   await app.listen(3004);
