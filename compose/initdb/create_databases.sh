807451e05d3283407b72822e5a8aad96c71b08fd
bouba-coder <boubacaryatera1@gmail.com>
2023-04-15 23:19:02 +0200

    adding analytic db

--- compose/initdb/create_databases.sh
+++ compose/initdb/create_databases.sh
@@ -5,4 +5,5 @@ psql -v ON_ERROR_STOP=1 --username "postgres" <<-EOSQL
     CREATE DATABASE users;
     CREATE DATABASE training;
     CREATE DATABASE nutrition;
+    CREATE DATABASE analytic;
 EOSQL
