d13d1bc5342be4c8773b96651c57b0bd31439e63
Adam Sellin <adamsellin1914@gmail.com>
2023-04-24 00:04:54 +0200

    delete file create_database.sh

--- compose/initdb/create_databases.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "postgres" <<-EOSQL
-    CREATE DATABASE auth;
-    CREATE DATABASE users;
-    CREATE DATABASE training;
-    CREATE DATABASE nutrition;
-EOSQL
