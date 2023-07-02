bff4ee2597cf6e5fdc25a19d88d2d28532d7fe5d
bouba-coder <boubacaryatera1@gmail.com>
2023-07-02 02:50:22 +0200

    pushin all images in registry azure

--- analytic-api/src/services/config/database.providers.ts
+++ analytic-api/src/services/config/database.providers.ts
@@ -5,6 +5,7 @@ export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
     useFactory: (): Promise<typeof mongoose> =>
-      mongoose.connect('mongodb://mongo:27017/users'),
+      //mongoose.connect('mongodb://mongo:27017/users'),
+      mongoose.connect(new ConfigService().get('mongoDnsDb')),
   },
 ];
