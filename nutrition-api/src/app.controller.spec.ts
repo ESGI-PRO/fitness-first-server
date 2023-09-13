302036ae6e289c25462565dd1d5bdbea6b462573
bouba-coder <boubacaryatera1@gmail.com>
2023-09-13 23:29:38 +0200

    pushing test deploy

--- nutrition-api/src/app.controller.spec.ts
+++ nutrition-api/src/app.controller.spec.ts
@@ -14,7 +14,6 @@ describe('AppController', () => {
     appController = app.get<AppController>(AppController);
   });
 
-  //  testt
   describe('root', () => {
     it('should return "Hello World!"', () => {
       expect(appController.getHello()).toBe('Hello World!');
