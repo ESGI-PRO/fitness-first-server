c9469cc38a970d7a85d927c0397c68cc8d259696
bouba-coder <boubacaryatera1@gmail.com>
2023-09-14 12:04:59 +0200

    pushing gateway and user to aks

--- nutrition-api/src/app.controller.spec.ts
+++ nutrition-api/src/app.controller.spec.ts
@@ -14,7 +14,7 @@ describe('AppController', () => {
     appController = app.get<AppController>(AppController);
   });
 
-  //  testt
+  //  testt ---
   describe('root', () => {
     it('should return "Hello World!"', () => {
       expect(appController.getHello()).toBe('Hello World!');
