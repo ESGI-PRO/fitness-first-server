3814140ea7e1b3edf89e59ef34f7efb04813c277
bouba-coder <boubacaryatera1@gmail.com>
2023-09-14 23:46:32 +0200

    test push all images on acr 2

--- gateway-api/test/app.e2e-spec.ts
+++ gateway-api/test/app.e2e-spec.ts
@@ -16,7 +16,7 @@ describe('AppController (e2e)', () => {
   });
 
   it('/ (GET)', () => {
-    //test return push 2
+    //test return push 3
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
