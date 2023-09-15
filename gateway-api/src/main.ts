82855a17dd33dfe78390cc53a2b416aac4256acc
bouba-coder <boubacaryatera1@gmail.com>
2023-09-15 23:33:05 +0200

    aks act deploy 7

--- gateway-api/src/main.ts
+++ gateway-api/src/main.ts
@@ -39,6 +39,6 @@ async function bootstrap() {
   app.use(helmet());
   app.use(rawBodyMiddleware());
   await app.listen(new ConfigService().get('port'), '0.0.0.0');
-  // pushing to aks 
+  // pushing to aks ss
 }
 bootstrap();
